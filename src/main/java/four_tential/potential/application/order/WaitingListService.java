126a2604435724f8188b616df60e61a0061eca3a
김동진 <kimdj9790@naver.com>
2026-04-14 18:46:07 +0900

    주문 생성

--- src/main/java/four_tential/potential/application/order/WaitingListService.java
+++ src/main/java/four_tential/potential/application/order/WaitingListService.java
@@ -6,14 +6,10 @@ import four_tential.potential.domain.order.WaitingListRepository;
 import four_tential.potential.infra.redis.RedisConstants;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
